6f143a33f4d5662bb5e9468a84b6530149006013
Rocha072 <matheusrochaalmeida7@gmail.com>
2025-10-22 02:01:29 -0300

    Main Menu completed

--- Assets/TD/Scripts/Game/GameMenus/MenuManagement.cs
+++ Assets/TD/Scripts/Game/GameMenus/MenuManagement.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuManagement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject panelMenuPrincipal;
+    public GameObject panelSelectLevel;
+    public GameObject panelGuide;
     void Start()
     {
-        
+        BackToMainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void BackToMainMenu()
     {
-        
+        panelMenuPrincipal.SetActive(true);
+        panelSelectLevel.SetActive(false);
+        panelGuide.SetActive(false);
+    }
+
+    public void PlayButton()
+    {
+        panelMenuPrincipal.SetActive(false);
+        panelSelectLevel.SetActive(true);
+        panelGuide.SetActive(false);
+    }
+
+    public void GuideButton()
+    {
+        panelMenuPrincipal.SetActive(false);
+        panelSelectLevel.SetActive(false);
+        panelGuide.SetActive(true);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void SelectLevel(int LevelNumber)
+    {
+        SceneManager.LoadScene(LevelNumber);
     }
 }
