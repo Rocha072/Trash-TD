659da23ca6a3858980fd42d6e197e59ddf546bc2
Matheus Rocha <matheusrochaalmeida7@gmail.com>
2025-10-25 15:31:31 -0300

    info button implemetantion and encapsuling the managementUI method

--- Assets/TD/Scripts/Game/InGameManagement/UIManager.cs
+++ Assets/TD/Scripts/Game/InGameManagement/UIManager.cs
@@ -1,10 +1,11 @@
 using TMPro;
 using UnityEngine;
-
 public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI moneyText;
     public TextMeshProUGUI lifeText;
+    public MousePosition mouse;
+    public GameObject guidePanel;
 
     public static UIManager Instance { get; private set; }
 
@@ -20,6 +21,17 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (guidePanel.activeSelf)
+                hideGuidePanel();
+            else
+                showGuidePanel();
+        }
+    }
+    
     public void UpdateMoneyText()
     {
         moneyText.text = "" + PlayerEconomy.Instance.GetMoney().ToString();
@@ -28,5 +40,24 @@ public class UIManager : MonoBehaviour
     public void UpdateLifeText()
     {
         lifeText.text = "" + PlayerLife.Instance.GetLife().ToString();
+    }
+
+
+    public void PurchaseTower(int towerID)
+    {
+        if (PlayerEconomy.Instance.CanBuy(towerID))
+        {
+            mouse.setTowerToPlaceByID(towerID);
+        }
+    }
+
+    public void showGuidePanel()
+    {
+        guidePanel.SetActive(true);
+    }
+    
+    public void hideGuidePanel()
+    {
+        guidePanel.SetActive(false);
     }
 }
